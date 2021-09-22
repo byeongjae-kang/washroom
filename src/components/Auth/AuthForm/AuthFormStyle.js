bb82be3b5a8c6e5d1c0fe2f9062ddc355fe9ae3f
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-22 09:22:07 -0700

    fix error

--- src/components/Auth/AuthForm/AuthFormStyle.js
+++ src/components/Auth/AuthForm/AuthFormStyle.js
@@ -2,15 +2,15 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    alignItems:'center'
+    alignItems: "center"
   },
   title: {
+    width: 350,
     height: 50,
     padding: 5,
     fontSize: 30,
     fontWeight: "bold"
   }
 });
-
 
 export default styles;
