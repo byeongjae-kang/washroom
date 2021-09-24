05eafc0ff1c79b2c492c6a1ab9a13d0e1d76a8bc
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-23 21:09:20 -0700

    add styles to show user invalid input

--- src/components/UI/Auth/AuthInputStyle.js
+++ src/components/UI/Auth/AuthInputStyle.js
@@ -11,6 +11,23 @@ const styles = StyleSheet.create({
     padding: 15,
     shadowRadius: 1.41,
     elevation: 0.1
+  },
+  input_invalid: {
+    height: 50,
+    width: 350,
+    
+    borderColor: "red",
+    borderBottomWidth: 1,
+    borderRadius: 2,
+    padding: 15,
+    shadowRadius: 1.41,
+    elevation: 0.1
+  },
+  text_invalid: {
+    fontSize: 10,
+    color: "red",
+    marginBottom: 2,
+    paddingLeft: 15
   }
 });
 
