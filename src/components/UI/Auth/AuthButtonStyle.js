266cde4065edcd9fa8d3a8281f2f6e4e0257f47f
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-24 16:41:23 -0700

    refactor styles

--- src/components/UI/Auth/AuthButtonStyle.js
+++ src/components/UI/Auth/AuthButtonStyle.js
@@ -10,13 +10,44 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     alignItems: "center",
     justifyContent: "center",
+    borderRadius: 3
   },
-  buttonText: {
-    color: "white",
-    fontSize: 20,
-    fontWeight: "bold"
-  },
-})
+  google: {
+    height: 50,
+    width: 350,
+    backgroundColor: "#EFEFFF",
+    borderColor: "#fff",
+    borderWidth: 1,
+    marginBottom: 10,
+    borderRadius: 3,
+    alignItems: "center",
+    justifyContent: "center",
 
+  },
+  facebook: {
+    height: 50,
+    width: 350,
+    backgroundColor: "#4267B2",
+    borderColor: "#fff",
+    borderWidth: 1,
+    marginBottom: 10,
+    borderRadius: 3,
+    alignItems: "center",
+    justifyContent: "center",
+
+  },
+  apple: {
+    height: 50,
+    width: 350,
+    backgroundColor: "black",
+    borderColor: "#fff",
+    borderWidth: 1,
+    marginBottom: 10,
+    borderRadius: 3,
+    alignItems: "center",
+    justifyContent: "center",
+
+  },
+});
 
 export default styles;
