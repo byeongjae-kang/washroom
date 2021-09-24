99a1762ab90493c95bc93feec2f98ae43905a0d5
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-24 16:41:05 -0700

    refactor to render children of the button

--- src/components/UI/Auth/AuthButton.jsx
+++ src/components/UI/Auth/AuthButton.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { TouchableOpacity, Text } from "react-native";
+import { TouchableOpacity } from "react-native";
 import styles from "./AuthButtonStyle";
 
 export default function AuthButton(props) {
+  let buttonStyle = styles.button;
+
+  if (props.account === "Google") {
+    buttonStyle = styles.google;
+  }
+  if (props.account === "Facebook") {
+    buttonStyle = styles.facebook;
+  }
+  if (props.account === "Apple") {
+    buttonStyle = styles.apple;
+  }
+
   return (
-    <TouchableOpacity style={styles.button} onPress={props.onPress}>
-      <Text style={styles.buttonText}>{props.buttonText}</Text>
-    </TouchableOpacity>
+    <TouchableOpacity style={buttonStyle}>{props.children}</TouchableOpacity>
   );
 }
